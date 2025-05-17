8da0f2c8516a6742ffde0e4658862dd7823f4d04
richardimaoka <richard.s.imaoka@gmail.com>
2025-05-17 13:00:06 +0900

    rotating box

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
 import styles from "./page.module.css";
+import * as motion from "motion/react-client";
 
-export default function Home() {
-  return <div className={styles.page}></div>;
+export default function Page() {
+  const box = {
+    width: 100,
+    height: 100,
+    backgroundColor: "#ff0088",
+    borderRadius: 5,
+  };
+  return (
+    <motion.div
+      style={box}
+      animate={{ rotate: 360 }}
+      transition={{ duration: 1 }}
+    />
+  );
 }
