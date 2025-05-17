387dcd3f50b7f955468e0f8c35115e85aad7172e
richardimaoka <richard.s.imaoka@gmail.com>
2025-05-17 13:09:26 +0900

    circle

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,21 @@ import styles from "./page.module.css";
 import * as motion from "motion/react-client";
 
 export default function Page() {
-  const box = {
+  const ball = {
     width: 100,
     height: 100,
-    backgroundColor: "#ff0088",
-    borderRadius: 5,
+    backgroundColor: "#dd00ee",
+    borderRadius: "50%",
   };
   return (
-    <motion.button initial={{ scale: 0 }} animate={{ scale: 1 }}>
-      button
-    </motion.button>
+    <motion.div
+      initial={{ opacity: 0, scale: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.4,
+        scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
+      }}
+      style={ball}
+    />
   );
 }
